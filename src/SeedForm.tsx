2127c80aac70e047aaaf0ca0d5356164d016049b
adam <asamere@gmail.com>
2023-04-18 13:17:17 -0700

    make form work

--- src/SeedForm.tsx
+++ src/SeedForm.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import { crypto } from '@shapeshiftoss/hdwallet-native'
+
 interface FormState {
   email: string;
   password: string;
+  encryptedWallet: string;
+  mnemonic: string;
 }
 
 const { EncryptedWallet } = crypto
@@ -21,9 +24,9 @@ const decryptNativeWallet = async (
     throw new Error('An email and password are required to decrypt the wallet.')
   if (!encryptedWalletString) throw new Error('An encryptedWallet is required for decryption.')
   try {
-      const encryptedWallet = getEncryptedWallet()
-      await encryptedWallet.init(email, password, encryptedWalletString)
-      return encryptedWallet.decrypt()
+    const encryptedWallet = getEncryptedWallet()
+    await encryptedWallet.init(email, password, encryptedWalletString)
+    return encryptedWallet.decrypt()
   } catch (e) {
     throw new Error('Native wallet decryption failed: ' + e)
   }
@@ -33,42 +36,81 @@ const SeedForm: React.FC = () => {
   const [formData, setFormData] = useState<FormState>({
     email: "",
     password: "",
+    encryptedWallet: "",
+    mnemonic: "",
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
-
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleTextAreaChange = async (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    // Handle form submission logic here
     console.log("Form data submitted: ", formData);
+    const mnemonic = await decryptNativeWallet(formData.email, formData.password, formData.encryptedWallet)
+    console.log("mnemonic: ", mnemonic)
+    setFormData({ ...formData, mnemonic: mnemonic || "" });
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <button type="submit">Submit</button>
+      <table>
+        <tr>
+          <td align="right">Email:</td>
+          <td align="left">
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td align="right">Password:</td>
+          <td align="left">
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td align="right">Encrypted Wallet:</td>
+          <td align="left">
+            <textarea
+              name="encryptedWallet"
+              value={formData.encryptedWallet}
+              onChange={handleTextAreaChange}
+              rows={5} // specify number of rows
+              cols={30} // specify number of columns
+            />
+          </td>
+        </tr>
+        <tr>
+          <td align="right">Mnemonic:</td>
+          <td align="left">
+          <textarea
+            name="mnemonic"
+            value={formData.mnemonic}
+            disabled={true}
+            rows={5} // specify number of rows
+            cols={30} // specify number of columns
+          />
+          </td>
+        </tr>
+        <tr>
+          <td/>
+          <td align="right"><button type="submit">Submit</button></td>
+        </tr>
+        
+      </table>
     </form>
   );
 };
